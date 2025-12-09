4214da9fe16b9c01ac9d8246d9b51c7924a199c6
Danielinh0 <danyeduardo0911@gmail.com>
2025-12-08 20:53:40 -0600

    Cambios para el deploy en Digital Ocean
    Seeder, Procfile, nginx

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             InicializadorSeeder::class,
-            HabilidadesSeeder::class,
-            LogrosSeeder::class,
         ]);
     }
 }
